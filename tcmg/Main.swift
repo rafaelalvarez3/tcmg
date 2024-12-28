79427b6c52a9bfecb8f9078fbc166cb77c142517
Rafael Alvarez <rafaelalvarez3@proton.me>
2024-12-28 10:04:33 -0600

    Reorganized the code for clarity.

--- tcmg/Main.swift
+++ tcmg/Main.swift
@@ -13,9 +13,17 @@ import TabularData
 
 @main
 struct Main: ParsableCommand {
-    static let configuration = CommandConfiguration(abstract:  "A utility to create and train Core ML models.")
+    static let configuration = CommandConfiguration(
+        abstract:  "A utility to create and train Core ML models."
+    )
     
-    @Option(name: [.customLong("trainfile"), .customShort("t")], help: "The file path of the training data file.")
+    @Option(
+        name: [
+            .customLong("trainfile"),
+            .customShort("t")
+        ],
+        help: "The path of the training file."
+    )
     var trainingDataFileName: String
     
     public func run() throws {
