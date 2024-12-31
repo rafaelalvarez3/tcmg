1cbb6d006f9c4ca0a65f14ed8b60f0b59b1be413
Rafael Alvarez <rafaelalvarez3@proton.me>
2024-12-31 16:59:48 -0600

    Moved the summary subcommand into its own file. Also removed the commented code.

---
+++ tcmg/Summary.swift
@@ -0,0 +1,40 @@
+//
+//  Summary.swift
+//  tcmg
+//
+//  Created by Rafael Alvarez on 12/31/24.
+//
+
+import Foundation
+import ArgumentParser
+import TabularData
+
+extension TCMG {
+    struct Summary: ParsableCommand {
+        
+        @OptionGroup var options: GlobalOptions
+        
+        public func run() throws {
+            
+            let csvOptions = CSVReadingOptions(
+                hasHeaderRow: true,
+                delimiter: ","
+            )
+            
+            var summaryDataFrame: DataFrame = DataFrame()
+            
+            do {
+                summaryDataFrame = try DataFrame(
+                    contentsOfCSVFile: newCSVFileURL(options.dataFileName),
+                    options: csvOptions
+                )
+            } catch UtilityError.fileWrongType {
+                print("ERROR: NOT A VALID CSV FILE")
+            } catch {
+                print("ERROR: AN UNKNOWN ERROR")
+            }
+            
+            print(summaryDataFrame.summary())
+        }
+    }
+}
