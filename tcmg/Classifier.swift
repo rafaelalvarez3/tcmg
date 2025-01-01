a50889c3413bba18545e75949628d3e8906898d9
Rafael Alvarez <rafaelalvarez3@proton.me>
2025-01-01 17:05:06 -0600

    Created the classifier command with its configuration.

--- tcmg/Classifier.swift
+++ tcmg/Classifier.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import ArgumentParser
+import TabularData
+import CreateML
+
+extension TCMG {
+    struct Classifier: ParsableCommand {
+        static let configuration = CommandConfiguration(
+            abstract: "This is the classfier command.",
+            discussion: "A longer description of this command that is shown in the help menu."
+        )
+    }
+}
