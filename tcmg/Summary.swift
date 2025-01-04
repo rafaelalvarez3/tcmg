f73feb6339654b4be1b54cd14b0b13e40bffa6ea
Rafael Alvarez <rafaelalvarez3@proton.me>
2025-01-04 10:50:29 -0600

    Reorganized summary command into subcommands. An "all" subcommand that prints the return of the summary method, and a "columns" subcommand that prints the columns.

--- tcmg/Summary.swift
+++ tcmg/Summary.swift
@@ -9,25 +9,50 @@ import TabularData
 extension TCMG {
     struct Summary: ParsableCommand {
         static let configuration = CommandConfiguration(
-            abstract: "Prints out a summary of the loaded data frame.",
-            discussion: "A longer description of this command that is shown in the help menu."
+            abstract: "Prints out summary information about the loaded data frame.",
+            discussion: "A longer description of this command that is shown in the help menu.",
+            subcommands: [All.self, ColumnInfo.self],
+            defaultSubcommand: All.self
         )
         
-        @OptionGroup var options: GlobalOptions
-        @Flag var columnNames: Bool = false
+        /* --------------------------------------------------------------------- */
         
-        public func run() throws {
-            let summaryDataFrame = try createCSVDataFrame(options.dataFileName)
+        struct All: ParsableCommand {
+            static let configuration = CommandConfiguration(
+                abstract: "Prints out a full summary of the loaded data frame."
+            )
             
-            if columnNames {
+            @OptionGroup var options: GlobalOptions
+            
+            public func run() throws {
+                let summaryDataFrame = try createCSVDataFrame(options.dataFileName)
+                print(summaryDataFrame.summary())
+            }
+            
+        }
+        
+        /* --------------------------------------------------------------------- */
+        
+        struct ColumnInfo: ParsableCommand {
+            static let configuration = CommandConfiguration(
+                commandName: "columns",
+                abstract: "Prints out the columns of the loaded data frame."
+            )
+            
+            @OptionGroup var options: GlobalOptions
+            
+            public func run() throws {
+                let summaryDataFrame = try createCSVDataFrame(options.dataFileName)
                 print(
                     summaryDataFrame.columns.map { colName in
                         colName.name
                     }
                 )
-            } else {
-                print(summaryDataFrame.summary())
             }
         }
+        
+        /* --------------------------------------------------------------------- */
     }
 }
+
+
